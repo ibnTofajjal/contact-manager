7709bc13ea452a07bc364c497b6e3d81ee8acb30
**##Joynal2003 <joynal.bintofajjal@gmail.com>
2023-03-22 04:00:56 +0600

    create a user with hashed password

--- .bashrc
+++
@@ -1,5 +0,0 @@
-function acp() {
-    git add .
-    git commit -a -m "$1"
-    git push
-}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,37 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
 // @ Register a user (POST /api/users/register) [public]
 const register = asyncHandler(async (req, res) => {
-  res.json({ success: true, msg: 'Register' });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('All fields are mandatory!');
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error('User already registered!');
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error('User data us not valid');
+  }
+  res.json({ message: 'Register the user' });
 });
 
 // @ Login a user (POST /api/users/login) [public]
